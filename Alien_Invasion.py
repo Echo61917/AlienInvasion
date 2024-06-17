ea757505f3d4e6a6d790c772f58b56a4e180ca2b
Echo61917 <98851748+Echo61917@users.noreply.github.com>
2024-06-17 10:28:54 -0700

    test

--- Alien_Invasion.py
+++ Alien_Invasion.py
@@ -18,20 +18,63 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        #Initializing background images
+        self.BG1 = self.settings.bg_expand_image1
+        self.BG2 = self.settings.bg_expand_image2
+        self.BG3 = self.settings.bg_expand_image3
+
+        #Image initial position and index
+        self.x = 0
+        self.i = 0
+
         self.ship = Ship(self)
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            #Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d:
+                    #Move the ship to the right
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_a:
+                    self.ship.moving_left = True
 
-            #Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            #Make the most recently drawn screen visible
-            pygame.display.flip()
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_d:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_a:
+                    self.ship.moving_left = False
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        # Redraw the screen during each pass through the loop.
+
+        # self.screen.fill(self.settings.bg_color) <---- This used to just fill the screen with the color I picked for background
+
+        # Incrementing the index for image swapping
+        self.i += 1
+        self.i = self.i % len(self.BG1)
+        self.i = self.i % len(self.BG2)
+        self.i = self.i % len(self.BG3)
+
+        # Drawing images at positions (x1,0), (x2,0), (x3,0)
+        self.screen.blit(self.BG1[self.i], (self.x, 0))
+        self.screen.blit(self.BG2[self.i], (self.x, 0))
+        self.screen.blit(self.BG3[self.i], (self.x, 0))
+
+        self.ship.blitme()
+
+        # Make the most recently drawn screen visible
+        # pygame.display.flip()
+        pygame.display.update()
+        #pygame.time.delay(150)  # setting delay for smooth animation
 
 if __name__ == '__main__':
     #Make a game instance, and run the game.
--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
+import pygame
+
 class Settings:
     """A class to store all settings for Alien Invasion"""
 
@@ -6,4 +8,19 @@ class Settings:
         #Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self. bg_color = (41, 3, 54)
+
+        #Load background images
+
+        self.bg_image1 = [pygame.image.load(f'images/BackGround 1 Frames/tile00{i}.png') for i in range(9)]
+        self.bg_image2 = [pygame.image.load(f'images/BackGround 2 Frames/tile00{i}.png') for i in range(9)]
+        self.bg_image3 = [pygame.image.load(f'images/BackGround 3 Frames/tile00{i}.png') for i in range(9)]
+
+        #Resize background image scale
+        self.bg_expand_image1 = [pygame.transform.scale_by(x, 2.25) for x in self.bg_image1]
+        self.bg_expand_image2 = [pygame.transform.scale_by(x, 2.25) for x in self.bg_image2]
+        self.bg_expand_image3 = [pygame.transform.scale_by(x, 2.25) for x in self.bg_image3]
+
+        #Ship settings
+        self.ship_speed = 1.5
+        #This value or field was what set the initial purple background color when I started
+        #self. bg_color = (41, 3, 54)
